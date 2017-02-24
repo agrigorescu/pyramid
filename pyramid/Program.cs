b3f756fc193f67ef34098c7c256928e4348f4bef
deea2 <deea2@ZOEPC>
2017-02-24 14:00:12 +0000

    adding the rest of the code

--- pyramid/Program.cs
+++ pyramid/Program.cs
@@ -11,12 +11,25 @@ namespace pyramid
         static void DrawPyramid(int n)
         {
             //some code will go here
-
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
         }
 
         //the point where the program is run
         static void Main(string[] args)
         {
+            DrawPyramid(4);
+            
         }
     }
 }
